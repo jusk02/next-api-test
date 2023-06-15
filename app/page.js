03ab047ffef0dfd5706eca6c1d3eb45683033997
Juan Ramirez <juramirez@hugeinc.com>
2023-06-15 12:32:58 -0500

    Basic boilerplate to create files via api with a button interaction

--- app/page.js
+++ app/page.js
@@ -1,13 +1,30 @@
+'use client';
 import Image from 'next/image'
 
+
 export default function Home() {
+  async function handleClick() {
+    console.log('increment like count');
+    const preformattedData = await fetch('api/banner');
+    const data = await preformattedData.json();
+    console.log(data);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <button
+          onClick={handleClick}
+          className="group rounded-lg border px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            test button to create banners
+        </button>
+      </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.js</code>
         </p>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
